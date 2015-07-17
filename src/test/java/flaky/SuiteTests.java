184d0e7291fe8bcd152595384a07f46df7ccd88a
Bogdan Livadariu <bogdan.livadariu@pure360.com>
2015-07-17 15:49:27 +0300

    trololo

---
+++ src/test/java/flaky/SuiteTests.java
@@ -0,0 +1,13 @@
+package flaky;
+
+import org.jenkinsci.testinprogress.runner.ProgressSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(ProgressSuite.class)
+@SuiteClasses({
+AppTest.class
+})
+public class SuiteTests {
+
+}
